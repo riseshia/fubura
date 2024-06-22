00da3347d4f93e31e1f7c4a1a3edce8872d7e7e9
Shia <rise.shia@gmail.com>
2024-06-22 09:32:49 +0900

    Add --ext-str option

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use clap::{Parser, Subcommand};
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
-#[command(propagate_version = true)]
+#[command(propagate_version = false)]
 struct Cli {
     #[command(subcommand)]
     command: Commands,
@@ -18,13 +18,39 @@ enum Commands {
         /// Config file
         #[arg(short, long, default_value = "fubura.jsonnet")]
         config: String,
+        #[clap(long = "ext-str", short = 'V', value_name = "key=[val]")]
+        ext_str: Vec<StrKeyVal>,
     },
     /// plan config
     Plan {
         /// Config file
         #[arg(short, long, default_value = "fubura.jsonnet")]
         config: String,
+        #[clap(long = "ext-str", short = 'V', value_name = "key=[val]")]
+        ext_str: Vec<StrKeyVal>,
     },
+}
+
+#[derive(Clone, Debug)]
+pub struct StrKeyVal {
+    pub var: String,
+    pub val: Option<String>,
+}
+
+impl From<&str> for StrKeyVal {
+    fn from(s: &str) -> Self {
+        if let Some((key, val)) = s.split_once('=') {
+            Self {
+                var: key.into(),
+                val: Some(val.into()),
+            }
+        } else {
+            Self {
+                var: s.into(),
+                val: None,
+            }
+        }
+    }
 }
 
 use fubura::commands::apply::ApplyCommand;
@@ -32,11 +58,14 @@ use fubura::commands::plan::PlanCommand;
 
 fn main() {
     let cli = Cli::parse();
+
     match &cli.command {
-        Commands::Apply { force, config } => {
+        Commands::Apply { force, config, ext_str } => {
+            println!("{:?}", ext_str);
             ApplyCommand::run(force, config);
         }
-        Commands::Plan { config } => {
+        Commands::Plan { config, ext_str } => {
+            println!("{:?}", ext_str);
             PlanCommand::run(config);
         }
     }
