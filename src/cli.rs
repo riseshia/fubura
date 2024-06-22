0b34f3ef06eadc96fbfd2c2054d9b1ca9bcc2296
Shia <rise.shia@gmail.com>
2024-06-22 09:38:16 +0900

    Extract cli to module

---
+++ src/cli.rs
@@ -0,0 +1,54 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+#[command(propagate_version = false)]
+pub struct Cli {
+    #[command(subcommand)]
+    pub command: Commands,
+}
+
+#[derive(Subcommand)]
+pub enum Commands {
+    /// apply config
+    Apply {
+        /// Skip to check changes, but only apply it.
+        #[arg(short, long)]
+        force: bool,
+        /// Config file
+        #[arg(short, long, default_value = "fubura.jsonnet")]
+        config: String,
+        #[clap(long = "ext-str", short = 'V', value_name = "key=[val]")]
+        ext_str: Vec<StrKeyVal>,
+    },
+    /// plan config
+    Plan {
+        /// Config file
+        #[arg(short, long, default_value = "fubura.jsonnet")]
+        config: String,
+        #[clap(long = "ext-str", short = 'V', value_name = "key=[val]")]
+        ext_str: Vec<StrKeyVal>,
+    },
+}
+
+#[derive(Clone, Debug)]
+pub struct StrKeyVal {
+    pub var: String,
+    pub val: Option<String>,
+}
+
+impl From<&str> for StrKeyVal {
+    fn from(s: &str) -> Self {
+        if let Some((key, val)) = s.split_once('=') {
+            Self {
+                var: key.into(),
+                val: Some(val.into()),
+            }
+        } else {
+            Self {
+                var: s.into(),
+                val: None,
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,6 @@
-use clap::{Parser, Subcommand};
+use clap::Parser;
 
-#[derive(Parser)]
-#[command(author, version, about, long_about = None)]
-#[command(propagate_version = false)]
-struct Cli {
-    #[command(subcommand)]
-    command: Commands,
-}
-
-#[derive(Subcommand)]
-enum Commands {
-    /// apply config
-    Apply {
-        /// Skip to check changes, but only apply it.
-        #[arg(short, long)]
-        force: bool,
-        /// Config file
-        #[arg(short, long, default_value = "fubura.jsonnet")]
-        config: String,
-        #[clap(long = "ext-str", short = 'V', value_name = "key=[val]")]
-        ext_str: Vec<StrKeyVal>,
-    },
-    /// plan config
-    Plan {
-        /// Config file
-        #[arg(short, long, default_value = "fubura.jsonnet")]
-        config: String,
-        #[clap(long = "ext-str", short = 'V', value_name = "key=[val]")]
-        ext_str: Vec<StrKeyVal>,
-    },
-}
-
-#[derive(Clone, Debug)]
-pub struct StrKeyVal {
-    pub var: String,
-    pub val: Option<String>,
-}
-
-impl From<&str> for StrKeyVal {
-    fn from(s: &str) -> Self {
-        if let Some((key, val)) = s.split_once('=') {
-            Self {
-                var: key.into(),
-                val: Some(val.into()),
-            }
-        } else {
-            Self {
-                var: s.into(),
-                val: None,
-            }
-        }
-    }
-}
-
+use fubura::cli::{Cli, Commands};
 use fubura::commands::apply::ApplyCommand;
 use fubura::commands::plan::PlanCommand;
 
