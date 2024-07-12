4c2f63fe7402fab9258105ad302ffbdffd4595c7
Shia <rise.shia@gmail.com>
2024-07-12 19:14:32 +0900

    Do not reuse plan command, just inline it

--- src/commands/apply.rs
+++ src/commands/apply.rs
@@ -1,17 +1,32 @@
 use std::process::exit;
 
 use crate::context::Context;
+use crate::differ::print_config_diff;
 use crate::types::SsConfig;
-
-use super::plan::PlanCommand;
+use crate::{scheduler, sfn, sts};
 
 pub struct ApplyCommand;
 
 impl ApplyCommand {
     pub async fn run(context: &Context, force: &bool, config: &SsConfig) {
-        if !force {
-            PlanCommand::run(context, config).await;
+        let sfn_arn_prefix = sts::build_sfn_arn_prefix(context).await;
+        let sfn_arn = format!("{}{}", sfn_arn_prefix, config.state.name);
 
+        let remote_sfn = sfn::describe_state_machine(&context.sfn_client, &sfn_arn).await;
+
+        let remote_schedule = if let Some(schedule_config) = &config.schedule {
+            scheduler::get_schedule(
+                &context.scheduler_client,
+                &schedule_config.schedule_name_with_group(),
+            )
+            .await
+        } else {
+            None
+        };
+
+        print_config_diff(config, &remote_sfn, &remote_schedule);
+
+        if !force {
             print!(
                 r#"
 Do you want apply this change?
@@ -24,7 +39,7 @@ Enter a value: "#
 
             if response != "yes" {
                 println!("apply cancelled!");
-                exit(1)
+                exit(1);
             }
         }
 
