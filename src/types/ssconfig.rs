15cc45a40b84617bc6eef91090f7319ee9787038
Shia <rise.shia@gmail.com>
2024-07-12 18:39:07 +0900

    Add delete flag to each resource

--- src/types/ssconfig.rs
+++ src/types/ssconfig.rs
@@ -2,8 +2,17 @@ use serde::{Deserialize, Serialize};
 
 use super::{Schedule, StateMachine};
 
+fn default_delete_flag() -> bool {
+    false
+}
+
 #[derive(Deserialize, Serialize, Debug)]
+#[serde(rename_all = "camelCase")]
 pub struct SsConfig {
     pub state: StateMachine,
     pub schedule: Option<Schedule>,
+    #[serde(default = "default_delete_flag")]
+    pub delete_state: bool,
+    #[serde(default = "default_delete_flag")]
+    pub delete_schedule: bool,
 }
