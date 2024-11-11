247f8cc650b2e9adff997e80a6cfd67191ecc31a
Shia <rise.shia@gmail.com>
2024-11-12 08:51:03 +0900

    Migration

--- src/jsonnet_evaluator.rs
+++ src/jsonnet_evaluator.rs
@@ -1,26 +1,36 @@
-use std::path::Path;
+use std::{collections::HashSet, path::Path};
 
 use serde_json::Value;
 
 use crate::cli::StrKeyVal;
 
+use rsjsonnet_front::Session;
 use rsjsonnet_lang::program::Value as JsonnetValue;
 
 pub fn eval(file_path: &str, ext_str: &[StrKeyVal]) -> Result<Value, String> {
     let source_path = Path::new(file_path);
 
-    let mut session = rsjsonnet_front::Session::new();
+    let arena = rsjsonnet_lang::arena::Arena::new();
+    let mut session = Session::new(&arena);
 
-    ext_str.iter().for_each(|ext_str| {
-        let key = session.program().str_interner().intern(&ext_str.var);
-        let val = if let Some(val) = &ext_str.val {
+    let mut ext_names = HashSet::new();
+
+    for arg in ext_str.iter() {
+        let key = session.program().intern_str(&arg.var);
+        if !ext_names.insert(key) {
+            let err_msg = format!("External variable {:?} defined more than once", arg.var);
+            return Err(err_msg);
+        }
+
+        let val = if let Some(val) = &arg.val {
             JsonnetValue::string(val.as_ref())
         } else {
             JsonnetValue::null()
         };
-        let value_thunk = session.program_mut().value_to_thunk(&val);
-        session.program_mut().add_ext_var(key, &value_thunk);
-    });
+
+        let thunk = session.program_mut().value_to_thunk(&val);
+        session.program_mut().add_ext_var(key, &thunk);
+    }
 
     let Some(thunk) = session.load_real_file(source_path) else {
         return Err("Failed to load file".to_string());
